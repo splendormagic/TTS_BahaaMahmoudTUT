f2b56bb6c14809a7f36e46b3bbf4ef60bee492ff
splendormagic <splendor.magic@gmail.com>
2024-04-15 22:11:58 +0200

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,8 +1,3 @@
-# Install the gTTS library if you haven't already
-!pip install gTTS
-!pip install pydub
-
-# Import the necessary module
 from gtts import gTTS
 from pydub import AudioSegment
 from IPython.display import Audio
